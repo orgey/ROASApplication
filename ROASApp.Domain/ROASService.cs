4361f25fa44e010478acb48434d26b707ac87a3b
orgey <anilorga@gmail.com>
2022-08-18 03:35:00 +0300

    Add project files.

---
+++ ROASApp.Domain/ROASService.cs
@@ -0,0 +1,78 @@
+﻿using ROASApp.Data.IO;
+using System.Text.Json;
+
+namespace ROASApp.Domain
+{
+    public class ROASService
+    {
+        private static List<ROAS> liste = new List<ROAS>();
+        public static ROAS SaveROAS(string reklamKanali, double reklamMaliyeti, double birimFiyat, int satisAdedi)
+        {
+            ROAS roas = new ROAS();
+            roas.reklamKanali = reklamKanali;
+            roas.reklamMaliyeti = reklamMaliyeti;
+            roas.birimFiyat = birimFiyat;
+            roas.satisAdedi = satisAdedi;
+            liste.Add(roas);
+            Write();
+
+            return roas;
+        }
+        public static ROAS EditROAS(string reklamKanali, double reklamMaliyeti, double birimFiyat, int satisAdedi, int kanalNo)
+        {
+            liste[kanalNo].reklamKanali = reklamKanali;
+            liste[kanalNo].reklamMaliyeti = reklamMaliyeti;
+            liste[kanalNo].birimFiyat = birimFiyat;
+            liste[kanalNo].satisAdedi = satisAdedi;
+            Write();
+
+            return liste[kanalNo];
+
+        }
+        public static IReadOnlyCollection<ROAS> GetAllROAS()
+        { 
+            LoadListFromFile(); 
+            return liste.AsReadOnly(); 
+        }
+        public static IReadOnlyCollection<ROAS> FilterByChannelName(string channelName)
+        {
+            LoadListFromFile();
+            List<ROAS> filteredROAS = new List<ROAS>();
+            foreach (ROAS r in liste)
+            {
+                if (r.reklamKanali.ToLower().Contains(channelName.ToLower()))
+                    filteredROAS.Add(r);
+            }
+            return filteredROAS.AsReadOnly();
+        }
+        public static void LoadListFromFile()
+        {
+            string json = FileOperation.Read();
+            liste = JsonSerializer.Deserialize<List<ROAS>>(json,
+                new JsonSerializerOptions { IncludeFields = true });
+            liste[0].SayacSifirlama();
+        }
+
+        public static ROAS GetSelectedROAS(int kanalNo)
+        {
+            LoadListFromFile();
+            return liste.ElementAt(kanalNo);
+
+        }
+        public static void Write()
+        {
+            string json = JsonSerializer.Serialize(liste,
+                new JsonSerializerOptions { IncludeFields = true });
+            FileOperation.Write(json);
+
+        }
+
+        public static void DeleteROAS(int indexNo)
+        {
+            liste.RemoveAt(indexNo);
+            Write();
+        }
+        //Todo:ROAS Silme işlevselliğini projeye ekleyin!
+        //Todo:ROAS Güncelleme işlevselliğini projeye ekleyin!
+    }
+}
---
+++ ROASApp.Presentation.ConsoleUI/Program.cs
@@ -0,0 +1,134 @@
+﻿using ROASApp.Domain;
+
+namespace ROASApp.Presentaion.ConsoleUI
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Menu();
+        }
+
+        private static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("1. Yeni ROAS Kaydı\n2. Roas Listesi\n3. ROAS Filtrele\n4. ROAS Duzenleme\n5. ROAS Silme\n6. Çıkış");
+            MenuSelection();
+        }
+        private static void MenuSelection()
+        {
+            Console.Write("Seçiminiz : ");
+            string choose = Console.ReadLine();
+            switch (choose)
+            {
+                case "1":
+                    NewROAS();
+                    break;
+                case "2":
+                    ListOfROAS();                    
+                    break;
+                case "3":
+                    FilterROAS();
+                    break;
+                case "4":
+                    EditROAS();
+                    break;
+                case "5":
+                    DeleteROAS();
+                    break;
+                case "6":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    MenuSelection();
+                    break;
+            }
+            Again();
+        }
+
+
+
+        static void Again()
+        {
+            Console.WriteLine("Menüye dönmek için Enter");
+            Console.ReadLine();
+            Menu();
+        }
+        private static void FilterROAS()
+        {
+            Console.WriteLine("Kanal adı içinde geçen kelimeyi yazın : ");
+            string filterKeyword = Console.ReadLine();
+            var data = ROASService
+                .FilterByChannelName(filterKeyword);
+            PrintList(data);
+        }
+        private static void DeleteROAS()
+        {
+            ListOfROAS();
+            Console.WriteLine("Silmek Istediginiz Kanal Numarasini Yazin : ");
+            int indexNo = Convert.ToInt32(Console.ReadLine());
+            ROASService.DeleteROAS(indexNo-1);
+            Console.WriteLine("Silme Isleminiz Basarili. Yeni Liste");
+            ListOfROAS();
+
+        }
+        private static void EditROAS()
+        {
+            ListOfROAS();
+            Console.WriteLine("Degistirmek Istediginiz Kanal Numarasini Yazin : ");
+            int kanalNo = (Convert.ToInt32(Console.ReadLine())-1);
+            var selectedROAS = ROASService.GetSelectedROAS(kanalNo);
+
+            Console.WriteLine($"Eski Reklam Kanalı Adı : {selectedROAS.reklamKanali}");
+            Console.WriteLine("Yeni Reklam Kanalı Adı :");
+
+            string kanalAdi = Console.ReadLine();
+            Console.WriteLine($"Eski Reklam Maliyeti : {selectedROAS.reklamMaliyeti}");
+            Console.WriteLine($"Yeni Reklam Maliyeti : ");
+            double maliyet = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Eski Satılan Ürünlerin Birim Fiyatı : {selectedROAS.birimFiyat}");
+            Console.WriteLine($"Yeni Satılan Ürünlerin Birim Fiyatı :");
+            double birimFiyat = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Eski Satılan Ürün Adedi : {selectedROAS.satisAdedi}");
+            Console.WriteLine($"Yeni Satılan Ürün Adedi : ");
+            int adet = Convert.ToInt32(Console.ReadLine());
+            
+            ROASService.EditROAS(kanalAdi, maliyet, birimFiyat, adet,kanalNo).ROASGetirisi();
+            Console.WriteLine("Degisiklikler Eklenmistir!");
+
+
+        }
+        private static void ListOfROAS()
+        {
+            var list = ROASService.GetAllROAS();
+            PrintList(list);
+        }
+        static void PrintList(IReadOnlyCollection<ROAS> list)
+        {
+            Console.WriteLine("----------- Liste Başlangıcı ----------");
+            foreach (ROAS r in list)
+            {
+                Console.WriteLine(r.ROASInfo());
+                Console.WriteLine("--------------------------------------");
+            }
+            Console.WriteLine("----------- Liste Sonu ----------");
+        }
+        private static void NewROAS()
+        {
+            Console.WriteLine("Reklam Kanalı Adı : ");
+            string kanalAdi = Console.ReadLine();
+            Console.WriteLine("Reklam Maliyeti : ");
+            double maliyet = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Satılan Ürünlerin Birim Fiyatı : ");
+            double birimFiyat = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Satılan Ürün Adedi : ");
+            int adet = Convert.ToInt32(Console.ReadLine());
+
+
+            var data = ROASService.SaveROAS(kanalAdi, maliyet, birimFiyat, adet);
+
+            Console.WriteLine($"Hesaplanan ROAS Değeri : %{data.ROASGetirisi()}");
+            
+        }
+    }
+}
